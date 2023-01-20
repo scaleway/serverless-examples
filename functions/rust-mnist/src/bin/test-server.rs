b294105c0a2c0642c19ff43b98bc945582438edf
Andy Méry <amery@scaleway.com>
2023-01-20 10:15:20 +0100

    fix(rust): switch to async handlers (#13)

--- functions/rust-mnist/src/bin/test-server.rs
+++ functions/rust-mnist/src/bin/test-server.rs
@@ -1,12 +1,9 @@
 use axum::{routing::post, Router};
-use tower_http::cors::CorsLayer;
 
 #[tokio::main]
 async fn main() {
-    let cors = CorsLayer::permissive();
     let app = Router::new()
-        .route("/", post(function::handler))
-        .layer(cors);
+        .route("/", post(function::handler));
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
