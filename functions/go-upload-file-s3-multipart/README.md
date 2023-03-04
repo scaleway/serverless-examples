04c8632e4dde04002b67a35e997674b43087c559
thomas-tacquet <thomas@tacquet.net>
2023-03-04 17:52:23 +0100

    Go multipart file + s3 storage

---
+++ functions/go-upload-file-s3-multipart/README.md
@@ -0,0 +1,36 @@
+# Go function to read file and upload to S3
+
+This function does the following steps :
+* Read a file from HTTP request
+* Save the file locally in ephemeral storage
+* Send file to S3 bucket
+
+Additionnaly it uses the [serverless-functions-go](https://github.com/scaleway/serverless-functions-go) library for local testing
+
+## Requirements
+
+If you want to enable S3 upload, ensure to have the following variables available in the environment:
+
+```
+S3_ENABLED=true
+S3_ENDPOINT= # ex: sample.s3.fr-par.scw.cloud
+S3_ACCESSKEY=
+S3_SECRET=
+S3_BUCKET_NAME=
+S3_REGION= # ex: fr-par
+```
+
+If s3 not enabled the file will be saved on the ephemeral storage of your function and will be lost.
+
+```sh
+go get
+
+# for local testing :
+go run cmd/main.go
+```
+
+To call the function (replace `go.sum` with the file you want to upload) : 
+
+```sh
+curl -X POST -H "Content-Type: multipart/form-data"  -F "data=@go.sum" http://localhost:8080
+```
---
+++ functions/go-upload-file-s3-multipart/main.go
@@ -0,0 +1,122 @@
+package handler
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+func Handle(w http.ResponseWriter, r *http.Request) {
+	// Entire files are sent as "multipart/form-data" so we need first to parse
+	// the request before accessing file content.
+	if err := r.ParseMultipartForm(32<<20 + 1024); err != nil {
+		panic(err)
+	}
+
+	// Save the multipartForm of the request.
+	multipartFile := r.MultipartForm.File["data"][0]
+
+	// Open the file.
+	file, err := multipartFile.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	// Here we read the whole file content into fileContent as byte slice,
+	// We can directly use the file descriptor to send it to s3 to avoid saving the file locally.
+	fileContent, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	// Reach the temp dir of system to save the file
+	tmpDir := os.TempDir()
+
+	// Build the temp file path
+	tempFile := filepath.Join(tmpDir, multipartFile.Filename)
+
+	// Write file to system
+	if err := os.WriteFile(tempFile, fileContent, 0o644); err != nil {
+		panic(err)
+	}
+
+	// Always remove the file
+	defer func() {
+		os.Remove(tempFile)
+	}()
+
+	// If S3 enabled, send the file
+	if strings.ToLower(os.Getenv("S3_ENABLED")) == "true" {
+		log.Println("S3 upload enabled")
+
+		if err := connectToS3AndPushFile(tempFile); err != nil {
+			panic(err)
+		}
+	} else {
+		log.Println("S3 upload disabled")
+	}
+}
+
+// connectToS3AndPushFile will push file in parameter to the S3 bucket. Env variables (secrets)
+// must be initialized to be able to upload files.
+func connectToS3AndPushFile(filePath string) error {
+	// Read required s3 variables
+	var (
+		endpoint        = os.Getenv("S3_ENDPOINT")
+		accessKeyID     = os.Getenv("S3_ACCESSKEY")
+		secretAccessKey = os.Getenv("S3_SECRET")
+		bucketName      = os.Getenv("S3_BUCKET_NAME")
+		region          = os.Getenv("S3_REGION")
+	)
+
+	// Initialize minio client object.
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: true,
+	})
+	if err != nil {
+		return fmt.Errorf("error opening s3 connexion : %w", err)
+	}
+
+	ctx := context.Background()
+
+	if err := minioClient.MakeBucket(ctx, bucketName, minio.MakeBucketOptions{Region: region}); err != nil {
+		exists, errBucketExists := minioClient.BucketExists(ctx, bucketName)
+		if errBucketExists == nil && exists {
+			log.Printf("%s already exists \n", bucketName)
+		} else {
+			return fmt.Errorf("error on checking bucket status : %w", err)
+		}
+	} else {
+		log.Printf("successfully created %s\n", bucketName)
+	}
+
+	const (
+		contentType = "application/octet-stream"
+	)
+
+	info, err := minioClient.FPutObject(
+		ctx,
+		bucketName,
+		filepath.Base(filePath),
+		filePath,
+		minio.PutObjectOptions{
+			ContentType: contentType,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("error on put object to S3 bucket : %w", err)
+	}
+
+	log.Printf("Successfully uploaded %s of size %d\n", filePath, info.Size)
+
+	return nil
+}
