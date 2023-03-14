fd38d993f4dcd6b33884cdb7ec2d5e2039b6b37e
Reda Noureddine <reda.noureddine.prepa@gmail.com>
2023-03-14 15:59:47 +0100

    feat(functions): add function handler

---
+++ functions/postgre-sql-python/handlers/handler.py
@@ -0,0 +1,53 @@
+import psycopg2
+from psycopg2 import Error
+import os
+import logging
+
+PG_HOST=os.environ.get('PG_HOST')
+PG_USER=os.environ.get('PG_USER')
+PG_DATABASE=os.environ.get('PG_DATABASE')
+PG_PASSWORD=os.environ.get('PG_PASSWORD')
+PG_PORT=os.environ.get('PG_PORT')
+
+def handle(event, context):
+
+    try:
+        connection = psycopg2.connect(
+            database=PG_DATABASE,
+            user=PG_USER,
+            host=PG_HOST,
+            password=PG_PASSWORD,
+            port=PG_PORT,
+            sslmode="disable")
+
+    except (Exception, Error) as error:
+        logging.error("Error while connecting to PostgreSQL database", error)
+        return {
+                "statusCode": 500,
+                "body": {
+                    "message": "Error while connecting to PostgreSQL database, check function logs for more information"
+                }
+        }
+
+    try:
+        cursor = connection.cursor()
+        logging.info("Connected to Database")
+        cursor.execute("SELECT * FROM table LIMIT 10")
+        record = cursor.fetchone()
+        logging.info("Successfully fetched data")
+        cursor.close()
+        return {
+                "statusCode": 200,
+                "body": {
+                    "message": record
+                }
+        }
+
+    except (Exception, Error) as error:
+        logging.error("Error while interacting with PostgreSQL", error)
+        return {
+                "statusCode": 500,
+                "body": {
+                    "message": "Error while getting information from PostgreSQL database, check function logs for more information"
+                }
+        }
