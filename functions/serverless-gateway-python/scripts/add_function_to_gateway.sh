eec834bb8610eb0258d6977b5aced4251e6bb33e
Reda Noureddine <reda.noureddine.prepa@gmail.com>
2023-04-03 13:43:56 +0200

    fix: update relative path in scripts

--- functions/serverless-gateway-python/scripts/add_function_to_gateway.sh
+++ functions/serverless-gateway-python/scripts/add_function_to_gateway.sh
@@ -4,4 +4,4 @@ set -e
 curl -X POST http://${GATEWAY_URL}/scw \
              -H 'X-Auth-Token: ${GATEWAY_TOKEN}' \
              -H 'Content-Type: application/json' \
-             -d '{"target":"$1","relative_url":"/$2"}'
+             -d '{"target":"$1","relative_url":"$2"}'
