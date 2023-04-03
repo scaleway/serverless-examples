8883f8ba592e92941334d13093e70bb0515ba858
Reda Noureddine <reda.noureddine.prepa@gmail.com>
2023-04-03 13:07:11 +0200

    chore: update script start

--- functions/serverless-gateway-python/scripts/add_function_to_gateway.sh
+++ functions/serverless-gateway-python/scripts/add_function_to_gateway.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -e
+
 curl -X POST http://${GATEWAY_URL}/scw \
              -H 'X-Auth-Token: ${GATEWAY_TOKEN}' \
              -H 'Content-Type: application/json' \
--- functions/serverless-gateway-python/scripts/list_gateway_endpoints.sh
+++ functions/serverless-gateway-python/scripts/list_gateway_endpoints.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+set -e
+
 curl http://${GATEWAY_URL}/scw -H 'X-Auth-Token: ${GATEWAY_TOKEN}'
