638a76331732ed44f982d06b07c73dc9bc36071f
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2023-04-11 16:22:09 +0200

    feat: PHP + S3 example (#32)
    * (feat): PHP and S3 example
    
    * Use TF for zipping
    
    * Style comments

---
+++ functions/php-s3/function/handler.php
@@ -0,0 +1,42 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+use Aws\S3\S3Client;
+
+function run($event, $context)
+{
+    // Get values from function environment variables
+    $s3_access_key = getenv('S3_ACCESS_KEY');
+    $s3_secret = getenv('S3_SECRET_KEY');
+    $s3_endpoint = getenv('S3_ENDPOINT');
+    $s3_region = getenv('S3_REGION');
+    $s3_bucket = getenv('S3_BUCKET');
+
+    $s3_key = 'example-key';
+
+    // Connect to S3
+    $s3 = new S3Client([
+        'region'      => $s3_region,
+        'version'     => 'latest',
+        'endpoint'    => $s3_endpoint,
+        'credentials' => [
+            'key'     => $s3_access_key,
+            'secret'  => $s3_secret,
+        ]
+    ]);
+
+    // Write to the key
+    $result = $s3->putObject([
+        'Bucket' => $s3_bucket,
+        'Key'    => $s3_key,
+        'Body'   => 'This is from the PHP example',
+    ]);
+
+    // Output the result of the S3 API operation
+    print_r($result);
+
+    return [
+        'body' => 'S3 function succeeded',
+    ];
+}
