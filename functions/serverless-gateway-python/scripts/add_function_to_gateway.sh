5853c2a8cc465737030e8355010e553e3babd94a
lucasscw <83953242+lucasscw@users.noreply.github.com>
2023-04-13 10:38:03 +0200

    fix environment variables to match readme

--- functions/serverless-gateway-python/scripts/add_function_to_gateway.sh
+++ functions/serverless-gateway-python/scripts/add_function_to_gateway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-curl -X POST https://${GATEWAY_URL}/scw \
-             -H 'X-Auth-Token: ${GATEWAY_TOKEN}' \
+curl -X POST https://${GATEWAY_HOST}/scw \
+             -H 'X-Auth-Token: ${TOKEN}' \
              -H 'Content-Type: application/json' \
              -d '{"target":"$1","relative_url":"$2"}'
