410e092732aee253d3d9f642551e4ba462478352
Sander Kooger <sander@thisisfashion.tv>
2023-04-24 09:48:01 +0200

    quoted out GW response

--- functions/nodejs-typescript-test/src/libs/api-gateway.ts
+++ functions/nodejs-typescript-test/src/libs/api-gateway.ts
@@ -1,12 +1,26 @@
-import type { APIGatewayProxyEvent, APIGatewayProxyResult, Handler } from "aws-lambda"
+import type {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Handler,
+} from "aws-lambda";
 import type { FromSchema } from "json-schema-to-ts";
 
-type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, 'body'> & { body: FromSchema<S> }
-export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<ValidatedAPIGatewayProxyEvent<S>, APIGatewayProxyResult>
+type ValidatedAPIGatewayProxyEvent<S> = Omit<APIGatewayProxyEvent, "body"> & {
+  body: FromSchema<S>;
+};
+export type ValidatedEventAPIGatewayProxyEvent<S> = Handler<
+  ValidatedAPIGatewayProxyEvent<S>,
+  APIGatewayProxyResult
+>;
 
-export const formatJSONResponse = (response: Record<string, unknown>) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(response)
-  }
+interface IformatJSONResponse {
+  statusCode: number;
+  response: Record<string, unknown>;
 }
+
+export const formatJSONResponse = (props: IformatJSONResponse) => {
+  return {
+    statusCode: props.statusCode || 200,
+    body: JSON.stringify(props.response),
+  };
+};
