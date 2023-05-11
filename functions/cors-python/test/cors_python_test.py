aa8b72a55ffb0445c9089b88d22dc6b1d308e9af
Reda Noureddine <reda.noureddine.prepa@gmail.com>
2023-05-11 16:46:37 +0200

    ci: python cors offline testing

--- functions/cors-python/test/cors_python_test.py
+++ functions/cors-python/test/cors_python_test.py
@@ -4,4 +4,8 @@ OFFLINE_TESTING_SERVER="http://localhost:8080"
 
 def test_handler_offline():
     response = requests.get(OFFLINE_TESTING_SERVER)
+    assert response.headers["Access-Control-Allow-Origin"] == "*"
+    assert response.headers["Access-Control-Allow-Headers"] == "*"
+    assert response.headers["Access-Control-Allow-Methods"] == "*"
+    assert response.headers["Content-Type"] == "text/plain"
     assert response.text == "This is checking CORS"
