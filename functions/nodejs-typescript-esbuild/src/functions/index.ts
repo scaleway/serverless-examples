105be3d501520f7fb6d1e0f186de2ce5bbaa44ff
Sander Kooger <sander@thisisfashion.tv>
2023-05-15 09:10:33 +0200

    added comment to /functions/index.ts

--- functions/nodejs-typescript-esbuild/src/functions/index.ts
+++ functions/nodejs-typescript-esbuild/src/functions/index.ts
@@ -1,6 +1,12 @@
 import hello2 from "./hello-2";
 import hello from "./hello";
 
+/**
+ * The idea for this file is to build a function that creates below output automagicly.
+ * The function shoudl scan all dubdirectories and then add import the function and add
+ * {"dirname": imported function} to below so that all functions can be exported automagicly.
+ */
+
 const functions = {
   hello: hello,
   "hello-2": hello2,
