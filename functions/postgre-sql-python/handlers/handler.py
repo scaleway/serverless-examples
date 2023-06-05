6971ae405a79d794866cd78f69426e8c57020fb7
Reda Noureddine <58738453+redanrd@users.noreply.github.com>
2023-06-05 10:33:55 +0200

    Ameliorate postgre sql python example (#41)
    Ameliorate postgre sql python example

--- functions/postgre-sql-python/handlers/handler.py
+++ functions/postgre-sql-python/handlers/handler.py
@@ -3,15 +3,13 @@ from psycopg2 import Error
 import os
 import logging
 
-PG_HOST=os.environ.get('PG_HOST')
-PG_USER=os.environ.get('PG_USER')
-PG_DATABASE=os.environ.get('PG_DATABASE')
-PG_PASSWORD=os.environ.get('PG_PASSWORD')
-PG_PORT=os.environ.get('PG_PORT')
-PG_SSL_ROOT_CERT=os.environ.get('PG_SSL_ROOT_CERT')
+PG_HOST = os.environ["PG_HOST"]
+PG_USER = os.environ["PG_USER"]
+PG_DATABASE = os.environ["PG_DATABASE"]
+PG_PASSWORD = os.environ["PG_PASSWORD"]
+PG_PORT = os.environ["PG_PORT"]
 
 def handle(event, context):
-
     try:
         connection = psycopg2.connect(
             database=PG_DATABASE,
@@ -19,42 +17,55 @@ def handle(event, context):
             host=PG_HOST,
             password=PG_PASSWORD,
             port=PG_PORT,
-            sslmode="require",
-            sslrootcert=PG_SSL_ROOT_CERT,
-            )
+            sslmode="disable",
+        )
+        logging.info("Connected to Database")
 
     except (Exception, Error) as error:
         logging.error("Error while connecting to PostgreSQL database", error)
         return {
-                "statusCode": 500,
-                "body": {
-                    "message": "Error while connecting to PostgreSQL database, check function logs for more information"
-                }
+            "statusCode": 500,
+            "body": {
+                "message": "Error while connecting to PostgreSQL database, check function logs for more information"
+            },
         }
 
     try:
-        cursor = connection.cursor()
-        logging.info("Connected to Database")
-        cursor.execute("SELECT * FROM table LIMIT 10")
-        record = cursor.fetchone()
-        logging.info("Successfully fetched data")
-        cursor.close()
-        return {
-                "statusCode": 200,
-                "body": {
-                    "message": record
-                }
-        }
+        sql_create_table = """CREATE TABLE dummy_table (
+            field_1 int,
+            field_2 varchar(20)
+        );"""
+
+        sql_insert_new_lines = """INSERT INTO dummy_table (field_1, field_2) 
+            VALUES
+            (10, 'dummy_data_1'),
+            (20, 'dummy_data_2');
+        """
+
+        with connection.cursor() as cursor:
+            logging.info("Successfully created database cursor")
+            cursor.execute(sql_create_table)
+            cursor.execute(sql_insert_new_lines)
+            connection.commit()
+            cursor.close()
+            logging.info("Successfully executed SQL queries")
+
+        connection.close()
+        logging.info("Database connection is closed")
+
+        return {"statusCode": 200, "body": {"message": "successful sql queries"}}
 
     except (Exception, Error) as error:
         logging.error("Error while interacting with PostgreSQL", error)
         return {
-                "statusCode": 500,
-                "body": {
-                    "message": "Error while getting information from PostgreSQL database, check function logs for more information"
-                }
+            "statusCode": 500,
+            "body": {
+                "message": "Error while getting information from PostgreSQL database, check function logs for more information"
+            },
         }
+
 
 if __name__ == "__main__":
     from scaleway_functions_python import local
+
     local.serve_handler(handle)
