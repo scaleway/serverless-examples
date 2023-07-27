ffdce2695e7d80b5376d38742be70a26b208f899
Emilie BOUIN <ebouin@scaleway.com>
2023-07-27 18:16:21 +0200

    test: add gha for go-upload-file-s3-multipart

---
+++ functions/go-upload-file-s3-multipart/handler_test.go
@@ -0,0 +1,46 @@
+package handler
+
+import (
+	"bytes"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandleWithCors(t *testing.T) {
+
+	offlineTestingServer := "http://localhost:8080"
+	fileToUpload := "go.sum"
+
+	form := new(bytes.Buffer)
+	writer := multipart.NewWriter(form)
+	fw, err := writer.CreateFormFile("data", filepath.Base(fileToUpload))
+	assert.Nil(t, err)
+
+	fd, err := os.Open(fileToUpload)
+	assert.Nil(t, err)
+
+	defer fd.Close()
+	_, err = io.Copy(fw, fd)
+	assert.Nil(t, err)
+
+	writer.Close()
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", offlineTestingServer, form)
+	assert.Nil(t, err)
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	resp, err := client.Do(req)
+	assert.Nil(t, err)
+
+	defer resp.Body.Close()
+
+	assert.Equal(t, 200, resp.StatusCode)
+
+}
