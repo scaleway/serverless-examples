697ebc1f4226fc053104578897c1f3cfb49f075e
Miguel Escribano <mescribano@scaleway.com>
2023-08-21 12:15:04 +0200

    Apply Black formatter

--- functions/python-sqs-trigger-async-worker/function/handler.py
+++ functions/python-sqs-trigger-async-worker/function/handler.py
@@ -5,16 +5,16 @@ import boto3
 
 
 def handle_front(event, context):
-    print('received request')
+    print("received request")
 
-    sqs_access_key = os.environ['SQS_ACCESS_KEY']
-    sqs_secret_key = os.environ['SQS_SECRET_KEY']
-    sqs_endpoint = os.environ['SQS_ENDPOINT']
-    sqs_queue_url = os.environ['SQS_QUEUE_URL']
-    sqs_region = os.environ['SQS_REGION']
+    sqs_access_key = os.environ["SQS_ACCESS_KEY"]
+    sqs_secret_key = os.environ["SQS_SECRET_KEY"]
+    sqs_endpoint = os.environ["SQS_ENDPOINT"]
+    sqs_queue_url = os.environ["SQS_QUEUE_URL"]
+    sqs_region = os.environ["SQS_REGION"]
 
     sqs = boto3.client(
-        'sqs',
+        "sqs",
         region_name=sqs_region,
         use_ssl=True,
         endpoint_url=sqs_endpoint,
@@ -22,17 +22,17 @@ def handle_front(event, context):
         aws_secret_access_key=sqs_secret_key,
     )
 
-    print(f'sending notification to queue {sqs_queue_url}')
+    print(f"sending notification to queue {sqs_queue_url}")
     try:
         sqs.send_message(
             QueueUrl=sqs_queue_url,
-            MessageBody=f'front received event at {datetime.datetime.now()}',
+            MessageBody=f"front received event at {datetime.datetime.now()}",
         )
     except Exception as e:
         print(e)
-        return {'statusCode': 500}
-    print('sent')
+        return {"statusCode": 500}
+    print("sent")
 
 
 def handle_worker(event, context):
-    print(f'worker received event: {event["body"]}')
+    print(f"worker received event: {event['body']}")
