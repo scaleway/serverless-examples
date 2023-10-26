e0d765e75d467a6fd03e013c75e989b28014b653
norbjd <norbjd@users.noreply.github.com>
2023-10-26 11:00:51 +0200

    Remove unnecessary wait between two triggers creation (#55)

--- containers/terraform-triggers/triggers.tf
+++ containers/terraform-triggers/triggers.tf
@@ -6,20 +6,10 @@ resource "scaleway_container_trigger" "public" {
   }
 }
 
-# There is a small bug today when multiple triggers are created at the same time
-# We'll wait 10 seconds before creating other triggers
-resource "time_sleep" "wait_10_seconds_after_public_trigger_creation" {
-  depends_on = [scaleway_container_trigger.public]
-
-  create_duration = "10s"
-}
-
 resource "scaleway_container_trigger" "private" {
   container_id = scaleway_container.private.id
   name         = "private-trigger"
   sqs {
     queue = scaleway_mnq_sqs_queue.private.name
   }
-
-  depends_on = [time_sleep.wait_10_seconds_after_public_trigger_creation]
 }
