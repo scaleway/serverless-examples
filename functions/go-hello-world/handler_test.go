86de4e4dd9458c1d55598620bb6151014c498ef6
Simon Shillaker <554768+Shillaker@users.noreply.github.com>
2023-11-06 15:07:06 +0100

    feat(functions): add Golang hello world example (#59)
    * Go hello world
    
    * Add golang hello world
    
    * Fix naming
    
    * Add to main README
    
    * Fix copy-paste error
    
    * Review comments
    
    * Remove zip

---
+++ functions/go-hello-world/handler_test.go
@@ -0,0 +1,22 @@
+package hello
+
+import (
+	"io"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const offlineTestingServer = "http://localhost:8080"
+
+func TestHelloWorld(t *testing.T) {
+	resp, err := http.Get(offlineTestingServer)
+	assert.NoError(t, err)
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := io.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	assert.Equal(t, "Hello from a Go function!", string(bodyBytes))
+}
