8e956b6b150926685ebc1ab6962b3c34e913d161
Reda Noureddine <reda.noureddine.prepa@gmail.com>
2024-04-17 16:37:29 +0200

    feat: model versioning

--- jobs/ml-ops/inference/loader.py
+++ jobs/ml-ops/inference/loader.py
@@ -4,6 +4,7 @@ import os
 
 class ClassifierLoader:
     _classifier = None
+    _classifier_version = ""
 
     @classmethod
     def load(cls, force=False):
@@ -23,14 +24,22 @@ class ClassifierLoader:
                 aws_secret_access_key=secret_key,
             )
 
-            # get file with the latest model version
+            # get model file with the latest version
             bucket_objects = s3.list_objects(Bucket=bucket_name)
-            get_last_modified = lambda obj: int(obj['LastModified'].strftime('%s'))
-            latest_model_file = [obj['Key'] for obj in sorted(bucket_objects, key=get_last_modified)][0]
+            get_last_modified = lambda object: int(object['LastModified'].strftime('%s'))
+            model_objects = [model_object for model_object in bucket_objects['Contents'] if "classifier" in model_object['Key']]
+            latest_model_file = [object['Key'] for object in sorted(model_objects, key=get_last_modified)][0]
 
             s3.download_file(bucket_name, latest_model_file, latest_model_file)
 
             with open(latest_model_file, "rb") as fh:
                 cls._classifier = pickle.load(fh)
+                cls._classifier_version = latest_model_file[11:-4]
+
+            print('Successfully loaded model file: {latest_model_file}'.format(latest_model_file=latest_model_file), flush=True)
 
         return cls._classifier
+
+    @classmethod
+    def model_version(cls):
+        return cls._classifier_version
