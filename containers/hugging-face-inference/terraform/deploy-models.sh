ff810088df0158c6d2099f58e2b1792a890ed8f8
Reda Noureddine <reda.noureddine.prepa@gmail.com>
2024-04-23 18:19:19 +0200

    feat: deploy models using json file info

--- containers/hugging-face-inference/terraform/deploy-models.sh
+++ containers/hugging-face-inference/terraform/deploy-models.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# Setup
+
 set -e
 
-export SCW_ACCESS_KEY=${SCW_ACCESS_KEY} \
-       SCW_SECRET_KEY=${SCW_SECRET_KEY} \
-       SCW_PROJECT_ID=${SCW_PROJECT_ID}
+export TF_VAR_access_key=${SCW_ACCESS_KEY} \
+       TF_VAR_secret_key=${SCW_SECRET_KEY} \
+       TF_VAR_project_id=${SCW_PROJECT_ID}
 
-declare -A hf_models 
+# Associative list of models to deploy
 
-hf_models["llama-2-7b.Q2_K.gguf"]="https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q8_0.gguf"
-hf_models["mistral-7b-instruct-v0.2.Q2_K.gguf"]="https://huggingface.co/TheBloke/Mistral-7B-Instruct-v0.2-GGUF/resolve/main/mistral-7b-instruct-v0.2.Q8_0.gguf"
+declare -A hf_models
+eval "$(jq -r '.[]|.[]|"hf_models[\(.file)]=\(.source)"' hf-models.json)"
+
+# Initialize, plan, and deploy each model in a Terraform workspace
 
 terraform init
 
@@ -18,9 +22,6 @@ do
   terraform workspace new $model_file_name
   export TF_VAR_hf_model_file_name=$model_file_name \
          TF_VAR_hf_model_download_source=${hf_models[$model_file_name]} \
-         TF_VAR_access_key=$SCW_ACCESS_KEY \
-         TF_VAR_secret_key=$SCW_SECRET_KEY \
-         TF_VAR_project_id=$SCW_PROJECT_ID
-  terraform plan -var-file=testing.tfvars
-  terraform apply -var-file=testing.tfvars -auto-approve
+  terraform plan
+  terraform apply -auto-approve
 done
