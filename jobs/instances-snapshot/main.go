dda5fd62be1184636c4882fd335d92556735b07f
Thomas Tacquet <thomas@tacquet.net>
2025-12-27 10:48:01 +0100

    feat(jobs): instances-snapshot improvements

--- jobs/instances-snapshot/main.go
+++ jobs/instances-snapshot/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 	"os"
 
 	"github.com/scaleway/scaleway-sdk-go/api/block/v1"
@@ -19,7 +21,21 @@ const (
 )
 
 func main() {
-	fmt.Println("creating snapshot of instance...")
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	if err := run(); err != nil {
+		slog.Error("application failed", "error", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	if err := checkEnvVars(); err != nil {
+		return err
+	}
+
+	slog.Info("creating snapshot of instance...")
 
 	// Create a Scaleway client with credentials from environment variables.
 	client, err := scw.NewClient(
@@ -32,52 +48,62 @@ func main() {
 		// Get more about our availability
 		// zones at https://www.scaleway.com/en/docs/console/my-account/reference-content/products-availability/
 		scw.WithDefaultRegion(scw.RegionFrPar),
+
+		scw.WithDefaultZone(scw.Zone(os.Getenv(envInstanceZone))),
 	)
 	if err != nil {
-		panic(err)
+		return fmt.Errorf("failed to create client: %w", err)
 	}
 
 	// Create SDK objects for Scaleway Instance product
 	instanceAPI := instance.NewAPI(client)
-
 	blockAPI := block.NewAPI(client)
 
-	if err := createSnapshots(instanceAPI, blockAPI); err != nil {
-		panic(err)
-	}
-}
+	ctx := context.Background()
 
-func createSnapshots(instanceAPI *instance.API, blockAPI *block.API) error {
-	gotInstance, err := instanceAPI.GetServer(&instance.GetServerRequest{
-		ServerID: os.Getenv(envInstanceID),
-		Zone:     scw.Zone(os.Getenv(envInstanceZone)),
-	})
-	if err != nil {
-		return fmt.Errorf("error while getting instance %w", err)
-	}
-
-	for _, volume := range gotInstance.Server.Volumes {
-		snapshotResp, err := blockAPI.CreateSnapshot(&block.CreateSnapshotRequest{
-			Zone:      scw.Zone(os.Getenv(envInstanceZone)),
-			VolumeID:  volume.ID,
-			ProjectID: os.Getenv(envProjectID),
-		})
-		if err != nil {
-			return fmt.Errorf("error while creating snapshot %w", err)
-		}
-
-		fmt.Println("created snapshot ", snapshotResp.ID)
+	if err := createSnapshots(ctx, instanceAPI, blockAPI); err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func init() {
-	mandatoryVariables := [...]string{envOrgID, envAccessKey, envSecretKey, envInstanceID, envInstanceZone}
+func createSnapshots(ctx context.Context, instanceAPI *instance.API, blockAPI *block.API) error {
+	zone := scw.Zone(os.Getenv(envInstanceZone))
+	instanceID := os.Getenv(envInstanceID)
 
-	for idx := range mandatoryVariables {
-		if os.Getenv(mandatoryVariables[idx]) == "" {
-			panic("missing environment variable " + mandatoryVariables[idx])
+	gotInstance, err := instanceAPI.GetServer(&instance.GetServerRequest{
+		ServerID: instanceID,
+		Zone:     zone,
+	}, scw.WithContext(ctx))
+	if err != nil {
+		return fmt.Errorf("error while getting instance: %w", err)
+	}
+
+	for _, volume := range gotInstance.Server.Volumes {
+		slog.Info("creating snapshot for volume", "volume_id", volume.ID)
+		snapshotResp, err := blockAPI.CreateSnapshot(&block.CreateSnapshotRequest{
+			Zone:      zone,
+			VolumeID:  volume.ID,
+			ProjectID: os.Getenv(envProjectID),
+		}, scw.WithContext(ctx))
+		if err != nil {
+			return fmt.Errorf("error while creating snapshot: %w", err)
+		}
+
+		slog.Info("created snapshot", "snapshot_id", snapshotResp.ID)
+	}
+
+	return nil
+}
+
+func checkEnvVars() error {
+	mandatoryVariables := []string{envOrgID, envAccessKey, envSecretKey, envInstanceID, envInstanceZone, envProjectID}
+
+	for _, v := range mandatoryVariables {
+		if os.Getenv(v) == "" {
+			return fmt.Errorf("missing environment variable: %s", v)
 		}
 	}
+	return nil
 }
